444306a15d5ba0b7a93a37e1fdc6a2cf343cf545
JimJr <golovko.alexei@gmail.com>
2017-03-23 01:41:38 +0200

    Provider class rename, initial commit

--- Daria/Repositories/MongoDbContext.cs
+++ Daria/Repositories/MongoDbContext.cs
@@ -1,9 +1,6 @@
 ﻿namespace Daria.Repositories
 {
-    using System;
-    using Daria.Models;
     using MongoDB.Driver;
-    using MongoDB.Bson;
     using System.Configuration;
 
     public class MongoDbContext
@@ -16,30 +13,6 @@
             this._dbClient = new MongoClient(
                 connectionString: ConfigurationManager.ConnectionStrings["Mongo"].ConnectionString);
             this._database = this._dbClient.GetDatabase("dariaDb");
-        }
-
-        public async User GetUser(string userId)
-        {
-            if (string.IsNullOrEmpty(userId))
-            {
-                throw new Exception("UserId cannot be null or empty.");
-            }
-
-            var users = this._database.GetCollection<BsonDocument>("users");
-            if(users == null)
-            {
-                return null;
-            }
-
-            var filter = Builders<BsonDocument>.Filter.Eq("userId", userId);
-            var result = await users.Find(filter).ToListAsync();
-
-            foreach (var doc in result)
-            {
-                foreach (var el in doc.Elements)
-                {                    
-                }
-            }
         }
     }
 }
--- runDb.bat
+++ runDb.bat
@@ -1,6 +1,6 @@
 @echo off
 cls
-title MondoRun
+title MongoRun
 echo Running mongo DB instance
 for /F "tokens=3 delims=: " %%H in ('sc query "MongoDb" ^| findstr "        STATE"') do (
   if /I "%%H" NEQ "RUNNING" (
