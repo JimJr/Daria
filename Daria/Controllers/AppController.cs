2feb968cc6a7119d20badc380d89d9e7c55b8390
JimJr <golovko.alexei@gmail.com>
2017-03-22 02:13:18 +0200

    Added app view and controller

---
+++ Daria/Controllers/AppController.cs
@@ -0,0 +1,12 @@
+﻿namespace Daria.Controllers
+{
+    using System.Web.Mvc;
+
+    public class AppController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
